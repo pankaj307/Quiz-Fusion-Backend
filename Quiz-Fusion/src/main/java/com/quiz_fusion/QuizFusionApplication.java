99c9192272c4d18e141cdb599dfdaecbc31d408b
pankaj-graas <pankaj.kushwah@graas.ai>
2024-11-30 17:42:03 +0530

    FirstCommit - Pushed project to github

---
+++ Quiz-Fusion/src/main/java/com/quiz_fusion/QuizFusionApplication.java
@@ -0,0 +1,13 @@
+package com.quiz_fusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizFusionApplication.class, args);
+	}
+
+}
---
+++ Quiz-Fusion/src/test/java/com/quiz_fusion/QuizFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz_fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
